db4a51b710d18ca1c1c8c366b1c9070cadd1003a
Seth Harrington <sbgha.09@gmail.com>
2024-09-03 21:59:10 -0600

    Subi referencia de de py7zr

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # EstructurasComputadoras2
 
+https://py7zr.readthedocs.io/en/latest/user_guide.html
 Install py7zr
 
 ```bash
--- proyecto1/predictors.py
+++ proyecto1/predictors.py
@@ -1,9 +1,13 @@
 import py7zr
 pathfile = "./proyecto1/traces.7z"
 
-
 with py7zr.SevenZipFile(pathfile,'r') as trace_fh:
+    trace_fh.extractall(path="./proyecto1/")
     for line in trace_fh:
         line = line.rstrip()
-        PC,result = line.split(" ")
-        print(PC, result)
+        print(line)
+# with py7zr.SevenZipFile(pathfile,'r') as trace_fh:
+#     for line in trace_fh:
+#         line = line.rstrip()
+#         PC,result = line.split(" ")
+#         print(PC, result)
